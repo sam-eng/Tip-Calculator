2ddc8ba6359c0b71d970fef1a86ff4ec89f74bb6
sam-eng <sme388@nyu.edu>
2019-08-31 15:16:38 -0400

    Initial Commit

---
+++ Tip Calculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Tip Calculator
+//
+//  Created by Samantha Eng on 8/31/19.
+//  Copyright © 2019 Samantha Eng. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
